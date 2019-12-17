712ce419c8b06ab468fa2af225f12f7296569f54
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-12-17 18:14:02 +0100

    Add settings scene

---
+++ src/scenes/settings_scene.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+
+import pygame
+import os
+from scenes import scene
+from scenes import game_scene
+from game_logic import tic_tac_toe_board
+from game_logic import helper
+
+
+class SettingsScene(scene.Scene):
+    """ Settings scene that enables configuring the game """
+
+    def __init__(self, director, past_scene):
+        scene.Scene.__init__(self, director)
+        self.__past_scene = past_scene
+        self.settings = None
+        # self.background = pygame.image.load(
+        #     os.getcwd()+'/scenes/images/intro.bmp')
+
+    def __go_back(self):
+        director.change_scene(self.__past_scene)
+
+    def read_settings(self, settings):
+        self.settings = settings
+
+    def on_event(self, events):
+        for event in events:
+            pass
+
+    def on_update(self):
+        if self.start_game:
+            scene = game_scene.GameScene(
+                self.director, *self.__start_new_game())
+            self.director.change_scene(scene)
+
+    def on_draw(self, screen):
+        screen.blit(self.background, (0, 0))
