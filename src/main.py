e79c90db5dcda56ff9b2b154659984db9c6f7663
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-12-18 15:37:39 +0100

    Create initial config when starting game

--- src/main.py
+++ src/main.py
@@ -3,14 +3,17 @@
 import pygame
 from scenes import director
 from scenes import intro_scene
+from game_logic import settings
 
 pygame.init()
 
 
 def main():
+    initial_settings = settings.Settings(
+        trials=1000, player='O', oponent='Computer')
     game_director = director.Director()
     scene = intro_scene.IntroScene(game_director)
-    game_director.change_scene(scene)
+    game_director.change_scene(scene, initial_settings)
     game_director.loop()
 
 
