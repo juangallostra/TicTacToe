1bd17e737f1dff27fac16fba01eb44310871d949
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-12-18 14:02:38 +0100

    Show settings menu

--- src/scenes/settings_scene.py
+++ src/scenes/settings_scene.py
@@ -1,9 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 import pygame
+import pygameMenu
 import os
 from scenes import scene, game_scene, intro_scene
-from game_logic import tic_tac_toe_board
+from game_logic import tic_tac_toe_board, settings_menu
 from game_logic import helper
 
 
@@ -15,6 +16,7 @@ class SettingsScene(scene.Scene):
         self.__past_scene = past_scene
         self.settings = None
         self.__go_back = False
+        self.__menu = settings_menu.SettingsMenu(self.director.screen)
         # self.background = pygame.image.load(
         #     os.getcwd()+'/scenes/images/intro.bmp')
 
@@ -25,15 +27,12 @@ class SettingsScene(scene.Scene):
         self.settings = settings
 
     def on_event(self, events):
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_b:
-                    self.__go_back = True
+        pass
 
     def on_update(self):
-        if self.__go_back:
-            self.director.change_scene(self.__past_scene, self.settings)
-            self.__go_back = False # reset state
+        self.__menu()
+        self.director.change_scene(self.__past_scene, self.settings)
+        self.__go_back = False  # reset state
 
     def on_draw(self, screen):
         screen.fill(helper.WHITE)
